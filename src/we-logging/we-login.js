27c4650a9a05a9d8e39048c7458d75cb7e49910b
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-03-14 09:21:43 +0200

    adding normal login

--- src/we-logging/we-login.js
+++ src/we-logging/we-login.js
@@ -5,14 +5,13 @@ const reqHandler = require("./module/handlers/request.handler");
 const resHandler = require("./module/handlers/response.handler");
 const emitter = require("./module/emitter/done");
 
-
 let config = {
   number: "",
   password: "",
   
 };
 
-const login = async () => {
+const loginWeb = async () => {
   return new Promise(async (resolve, reject) => {
     try {
       const browser = await puppeteer.launch({
@@ -37,5 +36,13 @@ const login = async () => {
       reject(error);
     }
   });
+};
+
+const login = async (number,password) => {
+  const preToken = "https://api-my.te.eg/api/user/generatetoken?channelId=WEB_APP"
+  const loginUrl = "`https://api-my.te.eg/api/user/login?channelId=WEB_APP`"
+
+  
+
 };
 module.exports = login;
