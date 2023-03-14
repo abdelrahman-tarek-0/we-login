cc71730e455a79e7d77b5152a69e415734907e86
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-03-14 09:17:04 +0200

    add we password encryption algorithm

---
+++ src/we-logging/utils/password-encryption.js
@@ -0,0 +1,30 @@
+// Node.js program to demonstrate the     
+// crypto.createDecipheriv() method
+
+// Includes crypto module
+const crypto = require('crypto');
+
+// Defining algorithm
+const algorithm = 'aes-128-cbc';
+const key = Buffer.from("0f0e0d0c0b0a09080706050403020100", "hex")
+const iv = Buffer.from("000102030405060708090a0b0c0d0e0f", "hex")
+
+// Creating decipher
+function encrypt(text) {
+
+    // Creating Cipheriv with its parameter
+    let cipher = crypto.createCipheriv(
+        algorithm, key, iv);
+
+    // Updating text
+    let encrypted = cipher.update(text);
+
+    // Using concatenation
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+    // Returning iv and encrypted data
+    return encrypted.toString('base64')
+
+}
+
+module.exports = encrypt
