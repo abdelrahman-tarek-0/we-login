60aad33a688ee540b668538d41e9542e0a1969d9
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-04-05 03:33:44 +0200

    fixed errro 👀

--- src/we-login.js
+++ src/we-login.js
@@ -1,3 +1,12 @@
+const puppeteer = require("puppeteer");
+const browserPath = require("get-browser-path");
+const axios = require("axios");
+
+const reqHandler = require("./module/handlers/request.handler");
+const resHandler = require("./module/handlers/response.handler");
+const emitter = require("./module/emitter/done");
+
+const encrypt = require("./utils/password-encryption");
 const puppeteer = require('puppeteer')
 const browserPath = require('get-browser-path')
 const reqHandler = require('./module/handlers/request.handler')
@@ -5,11 +14,12 @@ const resHandler = require('./module/handlers/response.handler')
 const emitter = require('./module/emitter/done')
 
 let config = {
-   number: '',
-   password: '',
-}
+  number: "",
+  password: "",
+};
 
-const login = async (opts = { browserPath: '' }) => {
+// this module is a f*cking piece of art
+const loginWeb = async (opts={webBrowserPath:''}) => {
    return new Promise(async (resolve, reject) => {
       try {
          const b = browserPath()
@@ -43,5 +53,63 @@ const login = async (opts = { browserPath: '' }) => {
          reject(error)
       }
    })
-}
-module.exports = login
+};
+
+const login = async (number, password,opts={skipEncryption:false}) => {
+  const preTokenUrl =
+    "https://api-my.te.eg/api/user/generatetoken?channelId=WEB_APP";
+  const loginUrl = "https://api-my.te.eg/api/user/login?channelId=WEB_APP";
+
+  if (!opts.skipEncryption) {
+    password = encrypt(password)
+  }
+  const payload = {
+    body: {
+      password: password,
+    },
+
+    header: {
+      msisdn: number,
+      numberServiceType: "FBB",
+      locale: "en",
+    },
+  };
+
+  const headers = {
+    Jwt: "",
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+    Host: "api-my.te.eg",
+    Origin: "https://my.te.eg",
+    Referer: "https://my.te.eg/",
+  };
+
+  const preTokenRes = await axios.get(preTokenUrl);
+
+  if (!preTokenRes?.data?.body?.jwt || !preTokenRes?.data?.body)
+    throw new Error("we server is down or not responding");
+
+  const preToken = preTokenRes?.data?.body?.jwt;
+  headers.Jwt = preToken;
+
+  let token = await axios.post(loginUrl, payload, {
+    headers,
+  });
+
+  if (!token?.data?.body?.jwt) throw new Error("wrong password or number");
+
+  customerId = token?.data?.header?.customerId;
+  customerName = token?.data?.body?.customerName;
+  token = token?.data?.body?.jwt;
+
+  return {number,password , token , customerId , customerName}
+};
+
+// login('022xxx','*********').then((data)=>{
+//   console.log(data);
+// })
+// loginWeb().then((data)=>{
+//   console.log(data);
+// })
+
+module.exports = {login,loginWeb};
